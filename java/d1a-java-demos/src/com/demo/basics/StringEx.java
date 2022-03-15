4110c423bfbad13feab3d7aae6adc82f5fe9cad1
DEV <dev@dev.com>
2022-03-15 18:16:57 +0530

    commit..

---
+++ java/d1a-java-demos/src/com/demo/basics/StringEx.java
@@ -0,0 +1,29 @@
+package com.demo.basics;
+
+public class StringEx {
+	public static void main(String[] args) {
+		String placeString = "Chennai";
+		System.out.println(placeString);
+		
+//		singleDimArray();
+		String seatArray[][] = new String[2][5];
+		seatArray[0][0] = "Dad";
+		seatArray[0][1]= "Mom";
+		seatArray[0][2]= "Son";
+		seatArray[0][3] = "Daughter";
+		seatArray[0][4] = "Pet Tommy";
+		
+		seatArray[1][0] = "Unlcle";
+		seatArray[1][1]= "Aunty";
+		seatArray[1][2]= "Uncle's Son";
+		seatArray[1][3] = "Uncle's Daughter";
+		seatArray[1][4] = "Pet Fluffy";
+		
+		for(int row = 0; row <=1; row++) {
+			for(int seat = 0; seat <=4; seat++) {
+				System.out.print(seatArray[row][seat] + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ java/lg-java.md
@@ -0,0 +1,3 @@
+:beginner: _**Resources**_  
+
+- docs https://docs.oracle.com/javase/8/docs/api/index.html?overview-summary.html
